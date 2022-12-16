63e47c837cab33e8c88e8b0d564748d581aed644
Chinmayi <phchinmayi93@gmail.com>
2022-12-16 21:28:02 +0530

    CURD

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const connectDB = require("./config/connectDB");
 
 const Task = require("./models/taskModel");
 
-const taskRoutes = require("./routes/taskRoute")
+const taskRoutes = require("./routes/taskRoute");
+
+const cors = require("cors")
+
 
 
 
@@ -21,6 +24,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors())
 app.use("/api/tasks",taskRoutes);
 
 
